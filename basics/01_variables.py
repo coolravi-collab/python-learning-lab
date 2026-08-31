7db361afb98c3c0e031050d768c11a04d6f252de
coolravi-collab <cool.raavs@gmail.com>
2026-08-30 22:32:42 -0700

    Create 01_variables.py
    Add variables exercise

---
+++ basics/01_variables.py
@@ -0,0 +1,16 @@
+# Excercise 01: Variables and basic data types
+
+name = "Python"
+year = "2026"
+version = 3.14
+learning = True
+
+print("Language:", name)
+print("Year:",year)
+print("Version:",version)
+print("Learning:",learning)
+
+print(type(name))
+print(type(year))
+print(type(version))
+print(type(learning))
